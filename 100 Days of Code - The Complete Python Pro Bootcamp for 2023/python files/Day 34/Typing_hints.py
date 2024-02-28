6734d4ff8cbd5ea8918cee7876ad5dc481663c5d
Subin-Vidhu <subinvidhu3232@gmail.com>
2024-02-28 06:20:11 +0530

    typing hints

---
+++ 100 Days of Code - The Complete Python Pro Bootcamp for 2023/python files/Day 34/Typing_hints.py
@@ -0,0 +1,20 @@
+age: int
+name: str
+height: float
+is_human: bool
+
+
+def police_check(age: int) -> bool:
+    if age > 18:
+        can_drive = True
+    else:
+        can_drive = False
+    return can_drive
+
+
+if police_check(12):
+    print("You may pass")
+else:
+    print("Pay a fine.")
+
+
