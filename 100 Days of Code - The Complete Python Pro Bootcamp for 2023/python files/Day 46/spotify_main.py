eda300a84138d885d38579976037acbadf3eab6b
Subin-Vidhu <subinvidhu3232@gmail.com>
2024-03-28 07:23:23 +0530

    spotify

---
+++ 100 Days of Code - The Complete Python Pro Bootcamp for 2023/python files/Day 46/spotify_main.py
@@ -0,0 +1,48 @@
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+
+sp = spotipy.Spotify(
+    auth_manager=SpotifyOAuth(
+        scope="playlist-modify-private",
+        redirect_uri="http://example.com",
+        client_id="YOUR UNIQUE CLIENT ID",
+        client_secret="YOUR UNIQUE CLIENT SECRET",
+        show_dialog=True,
+        cache_path="token.txt"
+    )
+)
+user_id = sp.current_user()["id"]
+
+# user_id = sp.current_user()["id"]
+date = input("Which year do you want to travel to? Type the date in this format YYYY-MM-DD: ")
+song_names = ["The list of song", "titles from your", "web scrape"]
+
+song_uris = []
+year = date.split("-")[0]
+for song in song_names:
+    result = sp.search(q=f"track:{song} year:{year}", type="track")
+    print(result)
+    try:
+        uri = result["tracks"]["items"][0]["uri"]
+        song_uris.append(uri)
+    except IndexError:
+        print(f"{song} doesn't exist in Spotify. Skipped.")
+
+sp = spotipy.Spotify(
+    auth_manager=SpotifyOAuth(
+        scope="playlist-modify-private",
+        redirect_uri="http://example.com",
+        client_id="YOUR UNIQUE CLIENT ID",
+        client_secret= "YOUR UNIQUE CLIENT SECRET",
+        show_dialog=True,
+        cache_path="token.txt"
+    )
+)
+user_id = sp.current_user()["id"]
+date = input("Which year do you want to travel to? Type the date in this format YYYY-MM-DD: ")
+song_uris = ["The list of", "song URIs", "you got by", "searching Spotify"]
+
+playlist = sp.user_playlist_create(user=user_id, name=f"{date} Billboard 100", public=False)
+# print(playlist)
+
+sp.playlist_add_items(playlist_id=playlist["id"], items=song_uris)
