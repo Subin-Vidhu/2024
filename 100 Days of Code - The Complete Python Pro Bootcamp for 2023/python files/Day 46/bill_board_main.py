5b613ca8e725e2ee0e9e5dfd6c721f856894b4fd
Subin-Vidhu <subinvidhu3232@gmail.com>
2024-03-28 07:23:15 +0530

    bill_board

---
+++ 100 Days of Code - The Complete Python Pro Bootcamp for 2023/python files/Day 46/bill_board_main.py
@@ -0,0 +1,10 @@
+from bs4 import BeautifulSoup
+import requests
+
+date = input("Which year do you want to travel to? Type the date in this format YYYY-MM-DD: ")
+
+response = requests.get("https://www.billboard.com/charts/hot-100/" + date)
+
+soup = BeautifulSoup(response.text, 'html.parser')
+song_names_spans = soup.select("li ul li h3")
+song_names = [song.getText().strip() for song in song_names_spans]
