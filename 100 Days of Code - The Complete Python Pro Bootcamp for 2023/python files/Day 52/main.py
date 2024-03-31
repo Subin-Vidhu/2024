cb48ed264a142724645022a705c8bf6b2265b9b0
Subin-Vidhu <subinvidhu3232@gmail.com>
2024-03-31 06:49:09 +0530

    main.py

--- 100 Days of Code - The Complete Python Pro Bootcamp for 2023/python files/Day 52/main.py
+++ 100 Days of Code - The Complete Python Pro Bootcamp for 2023/python files/Day 52/main.py
@@ -3,10 +3,10 @@ from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import ElementClickInterceptedException
 import time
 
-CHROME_DRIVER_PATH = YOUR CHROM DRIVER PATH
+CHROME_DRIVER_PATH = "YOUR CHROM DRIVER PATH"
 SIMILAR_ACCOUNT = "buzzfeedtasty"
-USERNAME = YOUR INSTAGRAM USERNAME
-PASSWORD = YOUR INSTAGRAM PASSWORD
+USERNAME = "YOUR INSTAGRAM USERNAME"
+PASSWORD = "YOUR INSTAGRAM PASSWORD"
 
 
 class InstaFollower:
