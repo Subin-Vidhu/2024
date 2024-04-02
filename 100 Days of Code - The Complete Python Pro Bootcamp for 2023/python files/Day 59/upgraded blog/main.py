6a66efc00659722c199764bd58d83978d85766d8
SUBIN S <subinvidhu3232@gmail.com>
2024-04-02 13:42:28 +0530

    Day 59

---
+++ 100 Days of Code - The Complete Python Pro Bootcamp for 2023/python files/Day 59/upgraded blog/main.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template
+import requests
+
+# USE YOUR OWN npoint LINK! ADD AN IMAGE URL FOR YOUR POST. 👇
+posts = requests.get("https://api.npoint.io/c790b4d5cab58020d391").json()
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def get_all_posts():
+    return render_template("index.html", all_posts=posts)
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+
+@app.route("/post/<int:index>")
+def show_post(index):
+    requested_post = None
+    for blog_post in posts:
+        if blog_post["id"] == index:
+            requested_post = blog_post
+    return render_template("post.html", post=requested_post)
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5001)
