f1af2af6b594d448381073aef397e0711e7778a0
SUBIN S <subinvidhu3232@gmail.com>
2024-04-03 17:07:27 +0530

    old

--- 100 Days of Code - The Complete Python Pro Bootcamp for 2023/python files/Day 68/main.py
+++ 100 Days of Code - The Complete Python Pro Bootcamp for 2023/python files/Day 68/main.py
@@ -3,115 +3,51 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin, login_user, LoginManager, login_required, current_user, logout_user
 
-
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'secret-key-goes-here'
 
-# CONNECT TO DB
+app.config['SECRET_KEY'] = 'any-secret-key-you-choose'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
-db = SQLAlchemy()
-db.init_app(app)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
-login_manager = LoginManager()
-login_manager.init_app(app)
-
-
-@login_manager.user_loader
-def load_user(user_id):
-    return db.get_or_404(User, user_id)
-
-
-# CREATE TABLE
+##CREATE TABLE IN DB
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(100), unique=True)
     password = db.Column(db.String(100))
     name = db.Column(db.String(1000))
-
-
-with app.app_context():
-    db.create_all()
+#Line below only required once, when creating DB. 
+# db.create_all()
 
 
 @app.route('/')
 def home():
-    return render_template("index.html", logged_in=current_user.is_authenticated)
+    return render_template("index.html")
 
 
-@app.route('/register', methods=["GET", "POST"])
+@app.route('/register')
 def register():
-    if request.method == "POST":
-
-        email = request.form.get('email')
-        result = db.session.execute(db.select(User).where(User.email == email))
-
-        # Note, email in db is unique so will only have one result.
-        user = result.scalar()
-        if user:
-            # User already exists
-            flash("You've already signed up with that email, log in instead!")
-            return redirect(url_for('login'))
-
-        hash_and_salted_password = generate_password_hash(
-            request.form.get('password'),
-            method='pbkdf2:sha256',
-            salt_length=8
-        )
-        new_user = User(
-            email=request.form.get('email'),
-            password=hash_and_salted_password,
-            name=request.form.get('name'),
-        )
-        db.session.add(new_user)
-        db.session.commit()
-        login_user(new_user)
-        return redirect(url_for("secrets"))
-
-    return render_template("register.html", logged_in=current_user.is_authenticated)
+    return render_template("register.html")
 
 
-@app.route('/login', methods=["GET", "POST"])
+@app.route('/login')
 def login():
-    if request.method == "POST":
-        email = request.form.get('email')
-        password = request.form.get('password')
-
-        result = db.session.execute(db.select(User).where(User.email == email))
-        user = result.scalar()
-        # Email doesn't exist or password incorrect.
-        if not user:
-            flash("That email does not exist, please try again.")
-            return redirect(url_for('login'))
-        elif not check_password_hash(user.password, password):
-            flash('Password incorrect, please try again.')
-            return redirect(url_for('login'))
-        else:
-            login_user(user)
-            return redirect(url_for('secrets'))
-
-    return render_template("login.html", logged_in=current_user.is_authenticated)
+    return render_template("login.html")
 
 
 @app.route('/secrets')
-@login_required
 def secrets():
-    print(current_user.name)
-    return render_template("secrets.html", name=current_user.name, logged_in=True)
+    return render_template("secrets.html")
 
 
 @app.route('/logout')
-@login_required
 def logout():
-    logout_user()
-    return redirect(url_for('home'))
+    pass
 
 
 @app.route('/download')
-@login_required
 def download():
-
-    # return send_from_directory('/static/files/', 'cheat_sheet.pdf')
-    return send_from_directory('static', path="files/cheat_sheet.pdf")
+    pass
 
 
 if __name__ == "__main__":
