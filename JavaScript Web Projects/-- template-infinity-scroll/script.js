1b03d524912f10302fe8ad64d95ec21a6d1b6aeb
Subin-Vidhu <subinvidhu3232@gmail.com>
2024-06-27 20:15:29 +0530

    .

--- JavaScript Web Projects/-- template-infinity-scroll/script.js
+++ JavaScript Web Projects/-- template-infinity-scroll/script.js
@@ -5,7 +5,7 @@ let photosArray = [];
 
 // Unsplash API
 const count = 10;
-const apiKey = 'PASTE_YOUR_API_KEY_HERE';
+const apiKey = '-KsOXvdzrmis5K5FFHbUNb1HOJks2OpVmYARueRdjAw';
 const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
 
 // Helper Function to Set Attributes on DOM Elements
