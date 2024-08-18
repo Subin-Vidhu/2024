f65efa946ea1d3e8fddf0922ea21eb9778c2c465
Subin-Vidhu <subinvidhu3232@gmail.com>
2024-08-18 06:17:53 +0530

    Create helper_functions.py

---
+++ AI Python for Beginners/Automating Tasks with Python/helper_functions.py
@@ -0,0 +1,77 @@
+import os
+
+from openai import OpenAI
+from dotenv import load_dotenv
+import csv
+
+# Get the OpenAI API key from the .env file
+load_dotenv('.env', override=True)
+openai_api_key = os.getenv('OPENAI_API_KEY')
+
+# Set up the OpenAI client
+client = OpenAI(api_key=openai_api_key)
+
+
+def print_llm_response(prompt):
+    """This function takes as input a prompt, which must be a string enclosed in quotation marks,
+    and passes it to OpenAI's GPT3.5 model. The function then prints the response of the model.
+    """
+    try:
+        if not isinstance(prompt, str):
+            raise ValueError("Input must be a string enclosed in quotes.")
+        completion = client.chat.completions.create(
+            model="gpt-3.5-turbo-0125",
+            messages=[
+                {
+                    "role": "system",
+                    "content": "You are a helpful but terse AI assistant who gets straight to the point.",
+                },
+                {"role": "user", "content": prompt},
+            ],
+            temperature=0.0,
+        )
+        response = completion.choices[0].message.content
+        print("_"*100)
+        print(response)
+        print("_"*100)
+        print("\n")
+    except TypeError as e:
+        print("Error:", str(e))
+
+
+def get_llm_response(prompt):
+    """This function takes as input a prompt, which must be a string enclosed in quotation marks,
+    and passes it to OpenAI's GPT3.5 model. The function then saves the response of the model as
+    a string.
+    """
+    completion = client.chat.completions.create(
+        model="gpt-3.5-turbo-0125",
+        messages=[
+            {
+                "role": "system",
+                "content": "You are a helpful but terse AI assistant who gets straight to the point.",
+            },
+            {"role": "user", "content": prompt},
+        ],
+        temperature=0.0,
+    )
+    response = completion.choices[0].message.content
+    return response
+
+
+def get_chat_completion(prompt, history):
+    history_string = "\n\n".join(["\n".join(turn) for turn in history])
+    prompt_with_history = f"{history_string}\n\n{prompt}"
+    completion = client.chat.completions.create(
+        model="gpt-3.5-turbo-0125",
+        messages=[
+            {
+                "role": "system",
+                "content": "You are a helpful but terse AI assistant who gets straight to the point.",
+            },
+            {"role": "user", "content": prompt_with_history},
+        ],
+        temperature=0.0,
+    )
+    response = completion.choices[0].message.content
+    return response
