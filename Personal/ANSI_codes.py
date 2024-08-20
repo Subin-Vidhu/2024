6a795c5d72f9987b84283de197373b6f6b04162c
Subin-Vidhu <subinvidhu3232@gmail.com>
2024-08-21 05:08:31 +0530

    Update ANSI_codes.py

---
+++ Personal/ANSI_codes.py
@@ -0,0 +1,4 @@
+print("\033[31mHello, world!\033[0m")  # Print "Hello, world!" in red
+print("\033[32mThis is green text.\033[0m")  # Print "This is green text." in green
+print("\033[1mThis is bold text.\033[0m")  # Print "This is bold text." in bold
+print("\033[4mThis is underlined text.\033[0m")  # Print "This is underlined text." with an underline
