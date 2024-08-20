73f547465ef031b4841b3fefc423838f6e0cc1bc
Subin-Vidhu <subinvidhu3232@gmail.com>
2024-08-21 04:56:58 +0530

    Update calendar_test.py

--- Personal/calendar_test.py
+++ Personal/calendar_test.py
@@ -1,3 +1,6 @@
 import calendar
-# print(f"All the dir in calendar module: {dir(calendar)}")
 
+month = 8
+year = 2024
+print(f"Calendar for {calendar.month_name[month]} {year}")
+print(calendar.month(year, month))
