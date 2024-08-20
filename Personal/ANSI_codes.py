9f6ce562b91e280ceee7d34548b6e6be778a262a
Subin-Vidhu <subinvidhu3232@gmail.com>
2024-08-21 05:09:43 +0530

    Update ANSI_codes.py

--- Personal/ANSI_codes.py
+++ Personal/ANSI_codes.py
@@ -1,4 +1,14 @@
-print("\033[31mHello, world!\033[0m")  # Print "Hello, world!" in red
-print("\033[32mThis is green text.\033[0m")  # Print "This is green text." in green
-print("\033[1mThis is bold text.\033[0m")  # Print "This is bold text." in bold
-print("\033[4mThis is underlined text.\033[0m")  # Print "This is underlined text." with an underline
+# print("\033[31mHello, world!\033[0m")  # Print "Hello, world!" in red
+# print("\033[32mThis is green text.\033[0m")  # Print "This is green text." in green
+# print("\033[1mThis is bold text.\033[0m")  # Print "This is bold text." in bold
+# print("\033[4mThis is underlined text.\033[0m")  # Print "This is underlined text." with an underline
+
+
+print("\033[30mBlack\033[0m")
+print("\033[31mRed\033[0m")
+print("\033[32mGreen\033[0m")
+print("\033[33mYellow\033[0m")
+print("\033[34mBlue\033[0m")
+print("\033[35mMagenta\033[0m")
+print("\033[36mCyan\033[0m")
+print("\033[37mWhite\033[0m")
