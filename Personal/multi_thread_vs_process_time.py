4db6ede6a5cf305a924aeb99cc7a33f2ccc82a3a
Subin-Vidhu <subinvidhu3232@gmail.com>
2024-08-26 20:09:23 +0530

    Revert "Update multi_thread_vs_process_time.py"
    This reverts commit 1993e4d5aa0237f2c5a1a8e9a2450abe287ad0b7.

--- Personal/multi_thread_vs_process_time.py
+++ Personal/multi_thread_vs_process_time.py
@@ -2,18 +2,22 @@ import threading
 import multiprocessing
 import time
 
-def cpu_intensive_task(n):
-    result = 0
-    for i in range(n):
-        result += i
-    return result
+def print_numbers():
+    for i in range(1, 11):
+        time.sleep(1)
+        print(i)
+
+def print_letters():
+    for letter in 'abcdefghij':
+        time.sleep(1)
+        print(letter)
 
 def multithreading_example():
     start_time = time.time()
 
     # Create threads
-    thread1 = threading.Thread(target=cpu_intensive_task, args=(10**8,))
-    thread2 = threading.Thread(target=cpu_intensive_task, args=(10**8,))
+    thread1 = threading.Thread(target=print_numbers)
+    thread2 = threading.Thread(target=print_letters)
 
     # Start threads
     thread1.start()
@@ -30,8 +34,8 @@ def multiprocessing_example():
     start_time = time.time()
 
     # Create processes
-    process1 = multiprocessing.Process(target=cpu_intensive_task, args=(10**8,))
-    process2 = multiprocessing.Process(target=cpu_intensive_task, args=(10**8,))
+    process1 = multiprocessing.Process(target=print_numbers)
+    process2 = multiprocessing.Process(target=print_letters)
 
     # Start processes
     process1.start()
