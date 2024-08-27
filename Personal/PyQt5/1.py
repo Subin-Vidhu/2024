2dd0455419cd76fa1c202c8efbb1ccfc9ef00de2
Subin-Vidhu <subinvidhu3232@gmail.com>
2024-08-27 05:31:18 +0530

    Update 1.py

--- Personal/PyQt5/1.py
+++ Personal/PyQt5/1.py
@@ -1,12 +1,12 @@
 import sys
 from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow, QLabel, QVBoxLayout
-
+from PyQt5.QtGui import QIcon
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Hello World")
         self.setGeometry(100, 100, 800, 600)
-
+        self.setWindowIcon(QIcon("icon.jpg"))
 
 def main():
     app = QApplication(sys.argv)
