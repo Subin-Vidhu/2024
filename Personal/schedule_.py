4931e682fe15c5cd63014af207edbadea4fe77f0
SUBIN S <subinvidhu3232@gmail.com>
2024-09-09 10:10:57 +0530

    sec & min

--- Personal/schedule_.py
+++ Personal/schedule_.py
@@ -2,27 +2,17 @@ import schedule
 import time
 from datetime import datetime
 
-def job():
+def job_every_sec():
     current_time = datetime.now().strftime("%H:%M:%S")
-    print(f"I'm working... Current time: {current_time}")
+    print(f"I'm working every sec... Current time: {current_time}")
 
-schedule.every(1).seconds.do(job)
+def job_every_min():
+    current_time = datetime.now().strftime("%H:%M:%S")
+    print(f"I'm working every min... Current time: {current_time}")
+
+schedule.every(1).seconds.do(job_every_sec)
+schedule.every(1).minutes.do(job_every_min)
 
 while True:
     schedule.run_pending()
     time.sleep(0.1)  # Run pending tasks more frequently
-# minute
-
-# import schedule
-# import time
-# from datetime import datetime
-
-# def job():
-#     current_time = datetime.now().strftime("%H:%M:%S")
-#     print(f"I'm working... Current time: {current_time}")
-
-# schedule.every().minute.do(job)
-
-# while True:
-#     schedule.run_pending()
-#     time.sleep(1)
