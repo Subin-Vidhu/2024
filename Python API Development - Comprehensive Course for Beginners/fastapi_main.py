93b982e28cd744534ba1292419e96c988e1f7a56
Subin-Vidhu <subinvidhu3232@gmail.com>
2024-09-18 07:14:57 +0530

    Update fastapi_main.py

--- Python API Development - Comprehensive Course for Beginners/fastapi_main.py
+++ Python API Development - Comprehensive Course for Beginners/fastapi_main.py
@@ -6,3 +6,8 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello Subin"}
+
+@app.get("/items")
+async def read_items():
+    #return a list of items
+    return [{"item": "item1"}, {"item": "item2"}]
