e8ad0d5fe3a389ac19309bf5317958372851be07
Subin-Vidhu <subinvidhu3232@gmail.com>
2024-09-23 19:22:14 +0530

    Create models.py

---
+++ Python API Development - Comprehensive Course for Beginners/app/models.py
@@ -0,0 +1,10 @@
+from .database import Base
+from sqlalchemy import Column, Integer, String, Boolean
+
+class Post(Base):
+    __tablename__ = "posts"
+    id = Column(Integer, primary_key=True, index=True, nullable=False)
+    title = Column(String, nullable=False)
+    content = Column(String, nullable=False)
+    published = Column(Boolean, default=True)
+    rating = Column(Integer, nullable=True)
