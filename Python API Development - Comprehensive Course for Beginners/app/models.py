764351d467e2b19c36244ee0d850260fd3ffdf29
Subin-Vidhu <subinvidhu3232@gmail.com>
2024-09-23 19:37:53 +0530

    Update models.py

--- Python API Development - Comprehensive Course for Beginners/app/models.py
+++ Python API Development - Comprehensive Course for Beginners/app/models.py
@@ -1,10 +1,13 @@
 from .database import Base
 from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy.sql.sqltypes import TIMESTAMP
+from sqlalchemy.sql.expression import text
 
 class Post(Base):
     __tablename__ = "posts"
     id = Column(Integer, primary_key=True, index=True, nullable=False)
     title = Column(String, nullable=False)
     content = Column(String, nullable=False)
-    published = Column(Boolean, default=True)
+    published = Column(Boolean, server_default='true')
     rating = Column(Integer, nullable=True)
+    created_at = Column(TIMESTAMP(timezone=True),nullable=False, server_default=text('now()'))
