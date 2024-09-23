0734ffe2955fa7f37567b8ee3e42a02fc6cae7f1
Subin-Vidhu <subinvidhu3232@gmail.com>
2024-09-23 19:46:35 +0530

    to return data from db using orm

--- Python API Development - Comprehensive Course for Beginners/app/main.py
+++ Python API Development - Comprehensive Course for Beginners/app/main.py
@@ -46,10 +46,11 @@ class Post(BaseModel):
 
 @app.get("/sqlalchemy")
 def read_sqlalchemy_posts(db: Session = Depends(get_db)):
-    posts = db.query(models.Post)
-    print(f"SQLAlchemy Posts: {posts}")
-    # return {"data": posts}
-    return {"data": "successfully fetched SQLAlchemy posts"}
+    # posts = db.query(models.Post)
+    # print(f"SQLAlchemy Posts: {posts}")
+    # return {"data": "successfully fetched SQLAlchemy posts"}
+    posts = db.query(models.Post).all()
+    return {"data": posts}
 
 # my_post = [{"title": "Post 1", "content": "This is the content of Post 1", "published": True, "rating": 5, "id" : 1},{"title": "Post 2", "content": "This is the content of Post 2", "published": False, "rating": 4, "id" : 2}, {"title": "Post 3", "content": "This is the content of Post 3", "published": True, "rating": 3, "id" : 3}]
 # Path Operation decorator/ Route / Endpoint
