ca69ae60b44c87620f70a14c187976fecfe8b104
Subin-Vidhu <subinvidhu3232@gmail.com>
2024-09-24 06:05:02 +0530

    Revert "adding created_at and id in the response"
    This reverts commit 7734dea10b8b01a831410a10398c896ddabf321f.

--- Python API Development - Comprehensive Course for Beginners/app/schemas.py
+++ Python API Development - Comprehensive Course for Beginners/app/schemas.py
@@ -1,5 +1,5 @@
 from pydantic import BaseModel
-from datetime import datetime
+
 # Pydantic Model
 class PostBase(BaseModel):
     title: str
@@ -11,11 +11,10 @@ class PostCreate(PostBase):
     pass
 
 class Post(BaseModel):
-    id: int
     title: str
     published: bool
     rating : int
-    created_at: datetime
+
 
     class Config:
         # orm_mode = True # Pydantic model to work with ORM
