c2c5eba228147e9bffd61477c1a64afaada1065d
SUBIN S <subinvidhu3232@gmail.com>
2024-09-24 08:34:29 +0530

    argon2

--- Personal/Hashing_methods.py
+++ Personal/Hashing_methods.py
@@ -5,6 +5,7 @@ Created on Tue Sep 24 08:30:03 2024
 @author: Subin-PC
 """
 
+'''
 #bcrypt
 import bcrypt
 import getpass
@@ -21,6 +22,45 @@ def verify_password(stored_password, provided_password):
     if bcrypt.checkpw(provided_password.encode('utf-8'), stored_password):
         return True
     else:
+        return False
+
+def main():
+    # Get the password from the user
+    password = getpass.getpass("Enter your password: ")
+
+    # Hash the password
+    hashed_password = hash_password(password)
+
+    print("Hashed Password: ", hashed_password)
+
+    # Verify the password
+    provided_password = getpass.getpass("Enter your password again: ")
+    if verify_password(hashed_password, provided_password):
+        print("Password is correct")
+    else:
+        print("Password is incorrect")
+
+if __name__ == "__main__":
+    main()
+'''
+
+# argon2
+import argon2
+from argon2 import PasswordHasher
+import getpass
+
+def hash_password(password):
+    ph = PasswordHasher()
+    hashed_password = ph.hash(password)
+
+    return hashed_password
+
+def verify_password(stored_password, provided_password):
+    ph = PasswordHasher()
+    try:
+        ph.verify(stored_password, provided_password)
+        return True
+    except:
         return False
 
 def main():
