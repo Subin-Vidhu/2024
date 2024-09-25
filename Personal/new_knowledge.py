8c2e9d2eac18ce5b3ca079860d403ac75915c253
SUBIN S <subinvidhu3232@gmail.com>
2024-09-25 09:01:54 +0530

    Create new_knowledge.py

---
+++ Personal/new_knowledge.py
@@ -0,0 +1,17 @@
+# # getpass module
+# import getpass
+
+# password = getpass.getpass("Enter your password: ")
+# print("You entered:", password)
+
+## Encrypting passwords is a common practice to protect sensitive information.
+# import string
+
+# punctuations = string.punctuation 
+# print(punctuations)
+
+# digits = string.digits
+# print(digits)
+
+# ascii_letters = string.ascii_letters
+# print(ascii_letters)
