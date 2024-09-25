2535bd32895f44bf791383148d8c5020313393c3
Subin-Vidhu <subinvidhu3232@gmail.com>
2024-09-25 07:32:38 +0530

    response_model

--- Python API Development - Comprehensive Course for Beginners/app/routers/auth.py
+++ Python API Development - Comprehensive Course for Beginners/app/routers/auth.py
@@ -7,7 +7,7 @@ router = APIRouter(
     tags=["authentications"]
 )
 
-@router.post("/login")
+@router.post("/login", response_model=schemas.Token)
 async def login_user(user_credentials: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
     # user = db.query(models.User).filter(models.User.email == user_credentials.email).first() # used when user_credentials: schemas.UserLogin
     user = db.query(models.User).filter(models.User.email == user_credentials.username).first() # used when user_credentials: OAuth2PasswordRequestForm
