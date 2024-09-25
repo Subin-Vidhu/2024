80cd05b249499ac2427efb04c00a836e6c9f2d8e
Subin-Vidhu <subinvidhu3232@gmail.com>
2024-09-25 07:01:41 +0530

    OAuth2PasswordRequestForm
    Switch request body from JSON to form data in Thunder Client

--- Python API Development - Comprehensive Course for Beginners/app/routers/auth.py
+++ Python API Development - Comprehensive Course for Beginners/app/routers/auth.py
@@ -2,14 +2,15 @@ from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.orm import Session
 from .. import schemas, models, utils, oauth2
 from ..database import get_db
-
+from fastapi.security import OAuth2PasswordRequestForm
 router = APIRouter(
     tags=["authentications"]
 )
 
 @router.post("/login")
-async def login_user(user_credentials: schemas.UserLogin, db: Session = Depends(get_db)):
-    user = db.query(models.User).filter(models.User.email == user_credentials.email).first()
+async def login_user(user_credentials: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
+    # user = db.query(models.User).filter(models.User.email == user_credentials.email).first() # used when user_credentials: schemas.UserLogin
+    user = db.query(models.User).filter(models.User.email == user_credentials.username).first() # used when user_credentials: OAuth2PasswordRequestForm
     if not user:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Invalid credentials")
     if not utils.verify(user_credentials.password, user.password):
