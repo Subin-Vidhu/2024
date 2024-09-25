e39af3ba9b7eb2afa9584c737d3209a0dc899218
Subin-Vidhu <subinvidhu3232@gmail.com>
2024-09-25 06:53:11 +0530

    creating token and checking it

--- Python API Development - Comprehensive Course for Beginners/app/routers/auth.py
+++ Python API Development - Comprehensive Course for Beginners/app/routers/auth.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.orm import Session
-from .. import schemas, models, utils
+from .. import schemas, models, utils, oauth2
 from ..database import get_db
 
 router = APIRouter(
@@ -15,4 +15,6 @@ async def login_user(user_credentials: schemas.UserLogin, db: Session = Depends(
     if not utils.verify(user_credentials.password, user.password):
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Incorrect password")
     # create and return the access token
-    return {"access_token": "example token"}
+    access_token = oauth2.create_access_token(data={"email": user.email, "id": user.id})
+    return {"access_token": access_token, "token_type": "bearer"}
+    # return {"access_token": "example token"}
