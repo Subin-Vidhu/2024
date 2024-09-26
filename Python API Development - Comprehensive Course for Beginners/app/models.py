fc13671efeda6aacd4667196e9145f1866b344f6
Subin-Vidhu <subinvidhu3232@gmail.com>
2024-09-26 05:46:52 +0530

    foreign key
    to set up relationship bw users and posts tables. For this to work, either drop the db and run the app again, or manually add the column using pgadmin for now.

--- Python API Development - Comprehensive Course for Beginners/app/models.py
+++ Python API Development - Comprehensive Course for Beginners/app/models.py
@@ -1,5 +1,5 @@
 from .database import Base
-from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
 from sqlalchemy.sql.sqltypes import TIMESTAMP
 from sqlalchemy.sql.expression import text
 
@@ -11,6 +11,7 @@ class Post(Base):
     published = Column(Boolean, server_default='true')
     rating = Column(Integer, nullable=True)
     created_at = Column(TIMESTAMP(timezone=True),nullable=False, server_default=text('now()'))
+    owner_id = Column(Integer, ForeignKey('users.id', ondelete="CASCADE"), nullable=False)
 
 class User(Base):
     __tablename__ = "users"
