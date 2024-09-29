766ad65b939dee4f714c2577a7f82bb3d1140d16
Subin-Vidhu <subinvidhu3232@gmail.com>
2024-09-29 09:44:17 +0530

    CORS

--- Python API Development - Comprehensive Course for Beginners/app/main.py
+++ Python API Development - Comprehensive Course for Beginners/app/main.py
@@ -2,11 +2,22 @@ from fastapi import FastAPI
 from . import models
 from .routers import post, users, auth, vote
 from .database import engine
-
+from fastapi.middleware.cors import CORSMiddleware
 # models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
+origins = [
+    "https://www.google.com"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 # my_post = [{"title": "Post 1", "content": "This is the content of Post 1", "published": True, "rating": 5, "id" : 1},{"title": "Post 2", "content": "This is the content of Post 2", "published": False, "rating": 4, "id" : 2}, {"title": "Post 3", "content": "This is the content of Post 3", "published": True, "rating": 3, "id" : 3}]
 # Path Operation decorator/ Route / Endpoint
 @app.get("/")
