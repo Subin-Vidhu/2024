5177dda6e7201892ac72b2a435bc68ce4debac8c
SUBIN S <subinvidhu3232@gmail.com>
2024-10-02 10:46:20 +0530

    today

--- Personal/spintly_daily_time_multiple_days.py
+++ Personal/spintly_daily_time_multiple_days.py
@@ -320,6 +320,6 @@ if __name__ == "__main__":
     year = 2024
     month = 10
     start_day = 1
-    end_day = 1
+    end_day = 2
     main(file_path, year, month, start_day, end_day)
     calculate_total_difference_from_csv(CSV_FILE_NAME)
