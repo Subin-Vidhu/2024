8099eec9bdc6b8084bfa6fb0dc4c0d9bdfdf79b9
Subin-Vidhu <subinvidhu3232@gmail.com>
2024-10-03 06:31:11 +0530

    bug in creating user already existing in db
    same user and password when used for creating again throws internal server error

--- Python API Development - Comprehensive Course for Beginners/app/routers/users.py
+++ Python API Development - Comprehensive Course for Beginners/app/routers/users.py
@@ -3,23 +3,37 @@ from ..database import get_db
 from sqlalchemy.orm import Session
 from fastapi import APIRouter, Depends, HTTPException, status, Response
 import psycopg2
+from sqlalchemy.exc import IntegrityError
 router = APIRouter(
     prefix="/users",
     tags=["users"]
 )
 
 # Create a user
-@router.post("/", response_model = schemas.UserOut)
-async def create_user(payload: schemas.createUser, db: Session = Depends(get_db)):
-    # Hash the password
-    hashed_password = utils.hash(payload.password)
-    payload.password = hashed_password
-    payload_dict = models.User(**payload.dict())
-    db.add(payload_dict)
-    db.commit()
-    db.refresh(payload_dict)
-    return payload_dict
 
+@router.post("/", response_model=schemas.UserOut)
+async def create_user(payload: schemas.createUser, db: Session = Depends(get_db)):
+    try:
+        # Hash the password
+        hashed_password = utils.hash(payload.password)
+        payload.password = hashed_password
+        payload_dict = models.User(**payload.dict())
+        db.add(payload_dict)
+        db.commit()
+        db.refresh(payload_dict)
+        return payload_dict
+    except IntegrityError as e:
+        db.rollback()
+        if "unique constraint" in str(e).lower():
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail="User with this username or email already exists.",
+            )
+        else:
+            raise HTTPException(
+                status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                detail="An internal server error occurred.",
+            )
 # Get only one user
 @router.get("/{id}", response_model = schemas.UserOut)
 async def read_user(id, response: Response, db: Session = Depends(get_db)):
