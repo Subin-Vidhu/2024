ee760631b3a5371a012eb7603744241965c57a7d
Subin-Vidhu <subinvidhu3232@gmail.com>
2024-10-05 05:10:45 +0530

    testing response

--- Python API Development - Comprehensive Course for Beginners/tests/test_users.py
+++ Python API Development - Comprehensive Course for Beginners/tests/test_users.py
@@ -12,7 +12,7 @@ from .database import client, session
 def test_create_user(client):
     res = client.post(
         "/users/", json={"email": "hel123@gmail.com", "password": "passrd123", "username": "hel123"})
-
+    print(f"Response: {res.json()}")
     new_user = schemas.UserOut(**res.json())
     assert new_user.email == "hel123@gmail.com"
     assert res.status_code == 200
