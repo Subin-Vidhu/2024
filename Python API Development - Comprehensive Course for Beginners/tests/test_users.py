a47ef37aa2686b101921b9e36791535b6c91f141
Subin-Vidhu <subinvidhu3232@gmail.com>
2024-10-04 07:57:01 +0530

    More Fixtures to handle database interaction

--- Python API Development - Comprehensive Course for Beginners/tests/test_users.py
+++ Python API Development - Comprehensive Course for Beginners/tests/test_users.py
@@ -1,44 +1,13 @@
-import pytest
-from fastapi.testclient import TestClient
-from app.main import app
 from app import schemas
-from app.config import settings
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.ext.declarative import declarative_base
-from app.database import get_db, Base
-# SQLALCHEMY_DATABASE_URL = "postgresql://postgres:password@localhost/FASTAPI" # Database URL
-SQLALCHEMY_DATABASE_URL = f"postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}/{settings.database_name}_test" # Database URL for testing
- 
-engine = create_engine(SQLALCHEMY_DATABASE_URL) # create a database engine
-
-TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine) # create a session
-
-# Base = declarative_base() # create a base class
-
-
-
-# Dependency
-def overrirde_get_db():
-    db = TestingSessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
-
-app.dependency_overrides[get_db] = overrirde_get_db
-
-@pytest.fixture
-def client():
-    Base.metadata.drop_all(bind=engine) # drop the database if it exists
-    Base.metadata.create_all(bind=engine) # create the database before the test
-    yield TestClient(app) # testing, yield helps in teardown
+from .database import client, session
 
 # def test_root(client):
+
 #     res = client.get("/")
 #     print(res.json().get('message'))
-#     assert res.json().get('message') == 'Hello Aramis'
+#     assert res.json().get('message') == 'Hello World'
 #     assert res.status_code == 200
+
 
 def test_create_user(client):
     res = client.post(
