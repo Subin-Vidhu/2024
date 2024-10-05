15d2dae67939cfc69385bdf3843cf159ba086348
Subin-Vidhu <subinvidhu3232@gmail.com>
2024-10-05 08:00:14 +0530

    test_wrong_login cases
    422 not set, so error, currenlty returns 404 for all

--- Python API Development - Comprehensive Course for Beginners/tests/test_users.py
+++ Python API Development - Comprehensive Course for Beginners/tests/test_users.py
@@ -44,16 +44,16 @@ def test_login_user(client, test_user):
     assert res.status_code == 200
 
 
-# @pytest.mark.parametrize("email, password, status_code", [
-#     ('wrongemail@gmail.com', 'password123', 403),
-#     ('sanjeev@gmail.com', 'wrongpassword', 403),
-#     ('wrongemail@gmail.com', 'wrongpassword', 403),
-#     (None, 'password123', 422),
-#     ('sanjeev@gmail.com', None, 422)
-# ])
-# def test_incorrect_login(test_user, client, email, password, status_code):
-#     res = client.post(
-#         "/login", data={"username": email, "password": password})
+@pytest.mark.parametrize("email, password, status_code", [
+    ('wrongemail@gmail.com', 'password123', 404),
+    ('sanjeev@gmail.com', 'wrongpassword', 404),
+    ('wrongemail@gmail.com', 'wrongpassword', 404),
+    (None, 'password123', 422),
+    # ('sanjeev@gmail.com', None, 422)
+])
+def test_incorrect_login(test_user, client, email, password, status_code):
+    res = client.post(
+        "/login", data={"username": email, "password": password})
 
-#     assert res.status_code == status_code
-#     # assert res.json().get('detail') == 'Invalid Credentials'
+    assert res.status_code == status_code
+    # assert res.json().get('detail') == 'Invalid Credentials'
