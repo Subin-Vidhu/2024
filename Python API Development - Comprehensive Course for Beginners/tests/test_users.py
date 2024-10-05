d3a78378176efebd5beb1bfbdd213fc14b1f43c3
Subin-Vidhu <subinvidhu3232@gmail.com>
2024-10-05 05:58:35 +0530

    Revert "create_user tested"
    This reverts commit 8c6d4d50b4f9cb5c6e85dbba8a092609e5cd347d.

--- Python API Development - Comprehensive Course for Beginners/tests/test_users.py
+++ Python API Development - Comprehensive Course for Beginners/tests/test_users.py
@@ -1,9 +1,5 @@
-import pytest
-from jose import jwt
 from app import schemas
-
-from app.config import settings
-
+from .database import client, session
 
 # def test_root(client):
 
@@ -15,40 +11,16 @@ from app.config import settings
 
 def test_create_user(client):
     res = client.post(
-        "/users/", json={
-  "username": "subin_test_create_user",
-  "email":"subin_test_create_user@gmail.com",
-  "password":"password"
-  
-})
-    print(f"res.json(): {res.json()}")
+        "/users/", json={"email": "hel123@gmail.com", "password": "passrd123", "username": "hel123"})
+    print(f"Response: {res.json()}")
     new_user = schemas.UserOut(**res.json())
-    assert new_user.email == "subin_test_create_user@gmail.com"
-    assert res.status_code == 201
-
-
-def test_login_user(test_user, client):
-    res = client.post(
-        "/login", data={"username": test_user['email'], "password": test_user['password']})
-    login_res = schemas.Token(**res.json())
-    payload = jwt.decode(login_res.access_token,
-                         settings.secret_key, algorithms=[settings.algorithm])
-    id = payload.get("user_id")
-    assert id == test_user['id']
-    assert login_res.token_type == "bearer"
+    assert new_user.email == "hel123@gmail.com"
     assert res.status_code == 200
 
-
-@pytest.mark.parametrize("email, password, status_code", [
-    ('wrongemail@gmail.com', 'password123', 403),
-    ('sanjeev@gmail.com', 'wrongpassword', 403),
-    ('wrongemail@gmail.com', 'wrongpassword', 403),
-    (None, 'password123', 422),
-    ('sanjeev@gmail.com', None, 422)
-])
-def test_incorrect_login(test_user, client, email, password, status_code):
+def test_login_user(client):
     res = client.post(
-        "/login", data={"username": email, "password": password})
-
-    assert res.status_code == status_code
-    # assert res.json().get('detail') == 'Invalid Credentials'
+        "/login", data={"username": "hel123@gmail.com", "password": "passrd123"})
+    print(f"Response: {res.json()}")
+    assert res.status_code == 200
+    assert res.json().get('access_token') is not None
+    print(f"Access Token: {res.json().get('access_token')}")
