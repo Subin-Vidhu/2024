0ec7dc1a40e13a8cf1bffaa65ccd1c172b15146a
SUBIN S <subinvidhu3232@gmail.com>
2024-10-05 12:16:32 +0530

    for testing pytest

--- Python API Development - Comprehensive Course for Beginners/tests/database.py
+++ Python API Development - Comprehensive Course for Beginners/tests/database.py
@@ -1,44 +1,44 @@
-from fastapi.testclient import TestClient
-import pytest
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.ext.declarative import declarative_base
-from app.main import app
+# from fastapi.testclient import TestClient
+# import pytest
+# from sqlalchemy import create_engine
+# from sqlalchemy.orm import sessionmaker
+# from sqlalchemy.ext.declarative import declarative_base
+# from app.main import app
 
-from app.config import settings
-from app.database import get_db
-from app.database import Base
-from alembic import command
-
-
-# SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:password123@localhost:5432/fastapi_test'
-SQLALCHEMY_DATABASE_URL = f'postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}_test'
+# from app.config import settings
+# from app.database import get_db
+# from app.database import Base
+# from alembic import command
 
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
-
-TestingSessionLocal = sessionmaker(
-    autocommit=False, autoflush=False, bind=engine)
+# # SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:password123@localhost:5432/fastapi_test'
+# SQLALCHEMY_DATABASE_URL = f'postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}_test'
 
 
-@pytest.fixture(scope="module")
-def session():
-    Base.metadata.drop_all(bind=engine)
-    Base.metadata.create_all(bind=engine)
-    db = TestingSessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
+# engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+# TestingSessionLocal = sessionmaker(
+#     autocommit=False, autoflush=False, bind=engine)
 
 
-@pytest.fixture(scope="module")
-def client(session):
-    def override_get_db():
+# @pytest.fixture(scope="module")
+# def session():
+#     Base.metadata.drop_all(bind=engine)
+#     Base.metadata.create_all(bind=engine)
+#     db = TestingSessionLocal()
+#     try:
+#         yield db
+#     finally:
+#         db.close()
 
-        try:
-            yield session
-        finally:
-            session.close()
-    app.dependency_overrides[get_db] = override_get_db
-    yield TestClient(app)
+
+# @pytest.fixture(scope="module")
+# def client(session):
+#     def override_get_db():
+
+#         try:
+#             yield session
+#         finally:
+#             session.close()
+#     app.dependency_overrides[get_db] = override_get_db
+#     yield TestClient(app)
