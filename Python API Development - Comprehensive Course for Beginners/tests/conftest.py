1dcc152934e4d96f5d02906abfd02182549b963e
Subin-Vidhu <subinvidhu3232@gmail.com>
2024-10-06 07:23:39 +0530

    all test works, but for easier github actions, commention out all

--- Python API Development - Comprehensive Course for Beginners/tests/conftest.py
+++ Python API Development - Comprehensive Course for Beginners/tests/conftest.py
@@ -1,140 +1,140 @@
-from fastapi.testclient import TestClient
-import pytest
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.ext.declarative import declarative_base
-from app.main import app
+# from fastapi.testclient import TestClient
+# import pytest
+# from sqlalchemy import create_engine
+# from sqlalchemy.orm import sessionmaker
+# from sqlalchemy.ext.declarative import declarative_base
+# from app.main import app
 
-from app.config import settings
-from app.database import get_db
-from app.database import Base
-from app.oauth2 import create_access_token
-from app import models
-from alembic import command
-
-
-# SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:password123@localhost:5432/fastapi_test'
-SQLALCHEMY_DATABASE_URL = f"postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}/{settings.database_name}_test" # Database URL
-
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
-
-TestingSessionLocal = sessionmaker(
-    autocommit=False, autoflush=False, bind=engine)
+# from app.config import settings
+# from app.database import get_db
+# from app.database import Base
+# from app.oauth2 import create_access_token
+# from app import models
+# from alembic import command
 
 
-@pytest.fixture()
-def session():
-    print("my session fixture ran")
-    Base.metadata.drop_all(bind=engine)
-    Base.metadata.create_all(bind=engine)
-    db = TestingSessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
+# # SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:password123@localhost:5432/fastapi_test'
+# SQLALCHEMY_DATABASE_URL = f"postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}/{settings.database_name}_test" # Database URL
+
+# engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+# TestingSessionLocal = sessionmaker(
+#     autocommit=False, autoflush=False, bind=engine)
 
 
-@pytest.fixture()
-def client(session):
-    def override_get_db():
+# @pytest.fixture()
+# def session():
+#     print("my session fixture ran")
+#     Base.metadata.drop_all(bind=engine)
+#     Base.metadata.create_all(bind=engine)
+#     db = TestingSessionLocal()
+#     try:
+#         yield db
+#     finally:
+#         db.close()
 
-        try:
-            yield session
-        finally:
-            session.close()
-    app.dependency_overrides[get_db] = override_get_db
-    yield TestClient(app)
+
+# @pytest.fixture()
+# def client(session):
+#     def override_get_db():
+
+#         try:
+#             yield session
+#         finally:
+#             session.close()
+#     app.dependency_overrides[get_db] = override_get_db
+#     yield TestClient(app)
 
 
-@pytest.fixture
-def test_user2(client):
-    user_data = {
-  "username": "subin_test_user2",
-  "email":"subin_test_user2@gmail.com",
-  "password":"password"
+# @pytest.fixture
+# def test_user2(client):
+#     user_data = {
+#   "username": "subin_test_user2",
+#   "email":"subin_test_user2@gmail.com",
+#   "password":"password"
   
-}
-    res = client.post("/users/", json=user_data)
+# }
+#     res = client.post("/users/", json=user_data)
 
-    assert res.status_code == 201
+#     assert res.status_code == 201
 
-    new_user = res.json()
-    new_user['password'] = user_data['password']
-    return new_user
+#     new_user = res.json()
+#     new_user['password'] = user_data['password']
+#     return new_user
 
 
-@pytest.fixture()
-def test_user(client):
-    user_data = {
-  "username": "subin_test_user",
-  "email":"subin_test_user@gmail.com",
-  "password":"password"
+# @pytest.fixture()
+# def test_user(client):
+#     user_data = {
+#   "username": "subin_test_user",
+#   "email":"subin_test_user@gmail.com",
+#   "password":"password"
   
-}
-    res = client.post("/users/", json=user_data)
+# }
+#     res = client.post("/users/", json=user_data)
 
-    assert res.status_code == 201
+#     assert res.status_code == 201
 
-    new_user = res.json()
-    new_user['password'] = user_data['password']
-    print(f"new_user: {new_user}")
-    return new_user
-
-
-@pytest.fixture
-def token(test_user):
-    print(f"test_user: {test_user}")
-    # print(f"create_access_token: {create_access_token({'email': test_user['email'], 'id': test_user['id']})}")
-    return create_access_token({'email': test_user['email'], 'id': test_user['id']})
+#     new_user = res.json()
+#     new_user['password'] = user_data['password']
+#     print(f"new_user: {new_user}")
+#     return new_user
 
 
-@pytest.fixture
-def authorized_client(client, token):
-    print(f"token: {token}")
-    client.headers = {
-        **client.headers,
-        "Authorization": f"Bearer {token}"
-    }
-    # print(f"client.headers: {client.headers, client}")
-    return client
+# @pytest.fixture
+# def token(test_user):
+#     print(f"test_user: {test_user}")
+#     # print(f"create_access_token: {create_access_token({'email': test_user['email'], 'id': test_user['id']})}")
+#     return create_access_token({'email': test_user['email'], 'id': test_user['id']})
 
 
-@pytest.fixture
-def test_posts(test_user, session, test_user2):
-    posts_data = [{
-        "title": "first title",
-        "content": "first content",
-        "owner_id": test_user['id'],
-        "rating": 0
-    }, {
-        "title": "2nd title",
-        "content": "2nd content",
-        "owner_id": test_user['id'],
-        "rating": 0
-    },
-        {
-        "title": "3rd title",
-        "content": "3rd content",
-        "owner_id": test_user['id'],
-        "rating": 0
-    }, {
-        "title": "3rd title",
-        "content": "3rd content",
-        "owner_id": test_user2['id'],
-        "rating": 0
-    }]
+# @pytest.fixture
+# def authorized_client(client, token):
+#     print(f"token: {token}")
+#     client.headers = {
+#         **client.headers,
+#         "Authorization": f"Bearer {token}"
+#     }
+#     # print(f"client.headers: {client.headers, client}")
+#     return client
 
-    def create_post_model(post):
-        return models.Post(**post)
 
-    post_map = map(create_post_model, posts_data)
-    posts = list(post_map)
+# @pytest.fixture
+# def test_posts(test_user, session, test_user2):
+#     posts_data = [{
+#         "title": "first title",
+#         "content": "first content",
+#         "owner_id": test_user['id'],
+#         "rating": 0
+#     }, {
+#         "title": "2nd title",
+#         "content": "2nd content",
+#         "owner_id": test_user['id'],
+#         "rating": 0
+#     },
+#         {
+#         "title": "3rd title",
+#         "content": "3rd content",
+#         "owner_id": test_user['id'],
+#         "rating": 0
+#     }, {
+#         "title": "3rd title",
+#         "content": "3rd content",
+#         "owner_id": test_user2['id'],
+#         "rating": 0
+#     }]
 
-    session.add_all(posts)
-    # session.add_all([models.Post(title="first title", content="first content", owner_id=test_user['id']),
-    #                 models.Post(title="2nd title", content="2nd content", owner_id=test_user['id']), models.Post(title="3rd title", content="3rd content", owner_id=test_user['id'])])
-    session.commit()
+#     def create_post_model(post):
+#         return models.Post(**post)
 
-    posts = session.query(models.Post).all()
-    print(f"posts: {posts}")
-    return posts
+#     post_map = map(create_post_model, posts_data)
+#     posts = list(post_map)
+
+#     session.add_all(posts)
+#     # session.add_all([models.Post(title="first title", content="first content", owner_id=test_user['id']),
+#     #                 models.Post(title="2nd title", content="2nd content", owner_id=test_user['id']), models.Post(title="3rd title", content="3rd content", owner_id=test_user['id'])])
+#     session.commit()
+
+#     posts = session.query(models.Post).all()
+#     print(f"posts: {posts}")
+#     return posts
