d54cca905ac4c3ef732da276407c8a1c36382fea
SUBIN S <subinvidhu3232@gmail.com>
2024-10-12 13:24:46 +0530

    turning code blocks into reusable functions

---
+++ AI Python for Beginners/Working with Your Own Data and Documents in Python/helper_functions (5).py
@@ -0,0 +1,63 @@
+import os
+
+from openai import OpenAI
+from dotenv import load_dotenv
+from datetime import datetime as dt
+
+
+# Get the OpenAI API key from the .env file
+load_dotenv('.env', override=True)
+openai_api_key = os.getenv('OPENAI_API_KEY')
+
+# Set up the OpenAI client
+client = OpenAI(api_key=openai_api_key)
+
+
+def print_llm_response(prompt):
+    """This function takes as input a prompt, which must be a string enclosed in quotation marks,
+    and passes it to OpenAI's GPT3.5 model. The function then prints the response of the model.
+    """
+    try:
+        if not isinstance(prompt, str):
+            raise ValueError("Input must be a string enclosed in quotes.")
+        completion = client.chat.completions.create(
+            model="gpt-4o-mini",
+            messages=[
+                {
+                    "role": "system",
+                    "content": "You are a helpful but terse AI assistant who gets straight to the point.",
+                },
+                {"role": "user", "content": prompt},
+            ],
+            temperature=0.0,
+        )
+        response = completion.choices[0].message.content
+        print(response)
+    except TypeError as e:
+        print("Error:", str(e))
+
+
+def get_llm_response(prompt):
+    """This function takes as input a prompt, which must be a string enclosed in quotation marks,
+    and passes it to OpenAI's GPT3.5 model. The function then saves the response of the model as
+    a string.
+    """
+    completion = client.chat.completions.create(
+        model="gpt-4o-mini",
+        messages=[
+            {
+                "role": "system",
+                "content": "You are a helpful but terse AI assistant who gets straight to the point.",
+            },
+            {"role": "user", "content": prompt},
+        ],
+        temperature=0.0,
+    )
+    response = completion.choices[0].message.content
+    return response
+
+
+def get_current_time():
+    now = dt.now()
+    return now.strftime("%m/%d/%Y, %H:%M:%S")
+
