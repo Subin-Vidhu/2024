80f3c6828a59a5757b50baa67cb74ae24376026a
Subin-Vidhu <subinvidhu3232@gmail.com>
2024-10-12 18:58:52 +0530

    Reapply "Reapply "Reapply "live share extension testing"""
    This reverts commit 91011c52b40052c065d92a931e0ee36c808be393.

---
+++ Personal/test_live_share.py
@@ -0,0 +1,11 @@
+# Encrypting passwords is a common practice to protect sensitive information.
+import string
+
+punctuations = string.punctuation 
+print(punctuations)
+
+digits = string.digits
+print(digits)
+
+ascii_letters = string.ascii_letters
+print(ascii_letters)
