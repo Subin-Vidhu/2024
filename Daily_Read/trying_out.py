03644e74e1be439f2c74f6ed1abd44e37b3bd42f
SUBIN S <subinvidhu3232@gmail.com>
2024-11-04 09:33:04 +0530

    One Hot Encoding

--- Daily_Read/trying_out.py
+++ Daily_Read/trying_out.py
@@ -5,6 +5,7 @@ Created on Mon Nov  4 08:54:29 2024
 @author: Aus
 """
 
+# Label Encoder
 from sklearn.preprocessing import LabelEncoder
 
 data = ['Red', 'Green', 'Blue', 'Red', 'Blue']
@@ -13,3 +14,39 @@ encoded_data = label_encoder.fit_transform(data)
 
 print(encoded_data)
 # Output: [2 1 0 2 0]
+
+
+# One Hot Encoding
+from sklearn.preprocessing import OneHotEncoder
+import numpy as np
+
+data = np.array(['Red', 'Green', 'Blue', 'Red', 'Blue']).reshape(-1, 1) 
+#.reshape(-1, 1):
+# Reshapes the array into a 2-dimensional array (matrix) with one column and as many rows as needed.
+# The -1 argument means "infer the number of rows from the length of the array," effectively turning the 1D array into a column vector.
+#The resulting shape of the array will be (5, 1), where 5 is the number of rows, and 1 is the number of columns.
+#  array([
+#     ['Red'],
+#     ['Green'],
+#     ['Blue'],
+#     ['Red'],
+#     ['Blue']
+# ])
+
+one_hot_encoder = OneHotEncoder(sparse=False) 
+# sparse=False:
+
+#The sparse parameter specifies the type of the matrix to return.
+#If sparse=True (the default), the encoder returns a sparse matrix. Sparse matrices are efficient for storing large, sparse datasets because they only store non-zero entries, saving memory.
+#If sparse=False, the encoder returns a dense NumPy array. Dense matrices store all entries, including zeros.
+# In this example, sparse=False is used, meaning the output will be a dense NumPy array
+
+encoded_data = one_hot_encoder.fit_transform(data)
+
+print(encoded_data)
+# Output:
+# [[0. 0. 1.]
+#  [0. 1. 0.]
+#  [1. 0. 0.]
+#  [0. 0. 1.]
+#  [1. 0. 0.]]
