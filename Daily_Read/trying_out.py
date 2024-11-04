532a3aeb68f2ef67b5e329cd25055d2ace05cd28
SUBIN S <subinvidhu3232@gmail.com>
2024-11-04 14:25:30 +0530

    RGB to grayscale

--- Daily_Read/trying_out.py
+++ Daily_Read/trying_out.py
@@ -174,3 +174,26 @@ print("Main thread terminated")
 # x.join()
 # y.join()
 # z.join()
+
+########################## RGB to grayscale #########################
+# Import the modules from skimage
+from skimage import data, color
+
+# Load the rocket image
+rocket = data.rocket()
+
+# Convert the image to grayscale
+gray_scaled_rocket = color.rgb2gray(rocket)
+
+# Show the original image
+show_image(rocket, 'Original RGB image')
+
+# Show the grayscale image
+show_image(gray_scaled_rocket, 'Grayscale image')
+
+import matplotlib.pyplot as plt
+def show_image(image, title='Image', cmap_type='gray'):
+    plt.imshow(image, cmap=cmap_type)
+    plt.title(title)
+    plt.axis('off')
+    plt.show()
