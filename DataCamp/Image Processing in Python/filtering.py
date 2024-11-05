46e8ee683ce3a4e71ba44f2a91c6dd1693cb2e0e
Subin-Vidhu <subinvidhu3232@gmail.com>
2024-11-05 06:46:07 +0530

    Equalize Histogram

--- DataCamp/Image Processing in Python/filtering.py
+++ DataCamp/Image Processing in Python/filtering.py
@@ -25,3 +25,20 @@ gaussian_image = gaussian(building_image, multichannel = True)
 # Show original and resulting image to compare
 show_image(building_image, "Original")
 show_image(gaussian_image, "Reduced sharpness Gaussian")
+
+#3. Equalize histogram
+# Import the required module
+from skimage import exposure
+
+# Show original x-ray image and its histogram
+show_image(chest_xray_image, 'Original x-ray')
+
+plt.title('Histogram of image')
+plt.hist(chest_xray_image.ravel(), bins=256)
+plt.show()
+
+# Use histogram equalization to improve the contrast
+xray_image_eq =  exposure.equalize_hist(chest_xray_image)
+
+# Show the resulting image
+show_image(xray_image_eq, 'Resulting image')
