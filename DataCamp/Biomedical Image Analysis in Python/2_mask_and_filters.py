92dd6c4f0eb02cf2b614a06b38ef2653155e2266
SUBIN S <subinvidhu3232@gmail.com>
2024-11-05 16:15:28 +0530

    apply a mask

--- DataCamp/Biomedical Image Analysis in Python/2_mask_and_filters.py
+++ DataCamp/Biomedical Image Analysis in Python/2_mask_and_filters.py
@@ -37,3 +37,20 @@ fig, axes = plt.subplots(1,2)
 axes[0].imshow(mask_skin, cmap='gray')
 axes[1].imshow(mask_bone, cmap='gray')
 format_and_render_plot()
+
+# Apply a mask
+# Import SciPy's "ndimage" module
+import scipy.ndimage as ndi
+
+# Screen out non-bone pixels from "im"
+mask_bone = im >= 145
+im_bone = np.where(mask_bone, im, 0)
+
+# Get the histogram of bone intensities
+hist = ndi.histogram(im_bone,1,255,255)
+
+# Plot masked image and histogram
+fig, axes = plt.subplots(2,1)
+axes[0].imshow(im_bone)
+axes[1].plot(hist)
+format_and_render_plot()
