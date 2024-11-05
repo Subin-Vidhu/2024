28c7af8bd83de5d59958977b48cba5cb5e6dbf2a
SUBIN S <subinvidhu3232@gmail.com>
2024-11-05 11:28:57 +0530

    Segmentation and face detection

--- DataCamp/Image Processing in Python/4_edges.py
+++ DataCamp/Image Processing in Python/4_edges.py
@@ -83,3 +83,19 @@ detected = detector.detect_multi_scale(img=friends_image,
                                        max_size=(200, 200))
 # Show the detected faces
 show_detected_face(friends_image, detected)
+
+# Segmentation and face detection
+# Obtain the segmentation with default 100 regions
+segments =  slic(profile_image, n_segments= 100)
+
+# Obtain segmented image using label2rgb
+segmented_image = label2rgb(segments, profile_image, kind='avg')
+
+# Detect the faces with multi scale method
+detected = detector.detect_multi_scale(img=segmented_image, 
+                                       scale_factor=1.2, 
+                                       step_ratio=1, 
+                                       min_size=(10, 10), max_size=(1000, 1000))
+
+# Show the detected faces
+show_detected_face(segmented_image, detected)
