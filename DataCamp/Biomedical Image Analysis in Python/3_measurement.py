7714be26d4e2bdd829275c52a31863677f0e9f7e
Subin-Vidhu <subinvidhu3232@gmail.com>
2024-11-05 19:34:03 +0530

    extract objects

--- DataCamp/Biomedical Image Analysis in Python/3_measurement.py
+++ DataCamp/Biomedical Image Analysis in Python/3_measurement.py
@@ -33,3 +33,33 @@ labels, nlabels = ndi.label(mask)
 # Create a mask for left ventricle pixels
 lv_val = labels[128, 128]
 lv_mask = np.where(labels == lv_val, 1, 0)
+
+# Create the labels array from mask
+labels, nlabels = ndi.label(mask)
+
+# Create a mask for left ventricle pixels
+lv_val = labels[128, 128]
+lv_mask = np.where(labels == lv_val, 1, 0)
+
+# Find bounding box of left ventricle
+bboxes = ndi.find_objects(lv_mask)
+print('Number of objects:', len(bboxes))
+print('Indices for first box:', bboxes[0])
+
+#
+# Create left ventricle mask
+labels, nlabels = ndi.label(mask)
+lv_val = labels[128, 128]
+lv_mask = np.where(labels == lv_val, 1, 0)
+
+# Find bounding box of left ventricle
+bboxes = ndi.find_objects(lv_mask)
+print('Number of objects:', len(bboxes))
+print('Indices for first box:', bboxes[0])
+
+#  Crop to the left ventricle (index 0)
+im_lv = im[bboxes[0]]
+
+# Plot the cropped image
+plt.imshow(im_lv, cmap='gray')
+format_and_render_plot()
