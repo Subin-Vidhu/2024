5f5b683643a914433098dc4628e0dcb120307318
Subin-Vidhu <subinvidhu3232@gmail.com>
2024-11-05 07:02:43 +0530

    resize

--- DataCamp/Image Processing in Python/filtering.py
+++ DataCamp/Image Processing in Python/filtering.py
@@ -93,3 +93,20 @@ rescaled_without_aa = rescale(rotated_cat_image, 1/4, anti_aliasing=False, multi
 # Show the resulting images
 show_image(rescaled_with_aa, "Transformed with anti aliasing")
 show_image(rescaled_without_aa, "Transformed without anti aliasing")
+
+#6. 3 times larger - resize
+# Import the module and function to enlarge images
+from skimage.transform import resize
+
+# Import the data module
+from skimage import data
+
+# Load the image from data
+rocket_image = data.rocket()
+
+# Enlarge the image so it is 3 times bigger
+enlarged_rocket_image = resize(rocket_image, (rocket_image.shape[0]*3, rocket_image.shape[1]*3), anti_aliasing=True)
+
+# Show original and resulting image
+show_image(rocket_image)
+show_image(enlarged_rocket_image, "3 times enlarged image")
