d9029b420a4aaf1656bdfb2572516f116a4ed8d9
SUBIN S <subinvidhu3232@gmail.com>
2024-11-05 14:24:58 +0530

    Final test

--- DataCamp/Image Processing in Python/4_edges.py
+++ DataCamp/Image Processing in Python/4_edges.py
@@ -116,3 +116,21 @@ for d in detected:
     # Merge this blurry face to our final image and show it
     resulting_image = mergeBlurryFace(group_image, blurred_face) 
 show_image(resulting_image, "Blurred faces")
+
+
+# Final
+# Import the necessary modules
+from skimage.restoration import denoise_tv_chambolle, inpaint
+from skimage import transform
+
+# Transform the image so it's not rotated
+upright_img = transform.rotate(damaged_image, 20)
+
+# Remove noise from the image, using the chambolle method
+upright_img_without_noise = denoise_tv_chambolle(upright_img,weight=0.1, multichannel=True)
+
+# Reconstruct the image missing parts
+mask = get_mask(upright_img)
+result = inpaint.inpaint_biharmonic(upright_img_without_noise, mask, multichannel=True)
+
+show_image(result)
