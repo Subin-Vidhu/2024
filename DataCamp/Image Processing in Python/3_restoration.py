6f09c00d2f2a410d0dbb6b9b3d4911a795d14592
SUBIN S <subinvidhu3232@gmail.com>
2024-11-05 09:24:26 +0530

    contours

--- DataCamp/Image Processing in Python/3_restoration.py
+++ DataCamp/Image Processing in Python/3_restoration.py
@@ -50,3 +50,16 @@ segmented_image = label2rgb(segments, face_image, kind='avg')
 
 # Show the segmented image
 show_image(segmented_image, "Segmented image, 400 superpixels")
+
+# Contours
+# Import the modules
+from skimage import data, measure
+
+# Obtain the horse image
+horse_image = data.horse()
+
+# Find the contours with a constant level value of 0.8
+contours = measure.find_contours(horse_image, 0.8)
+
+# Shows the image with contours found
+show_image_contour(horse_image, contours)
