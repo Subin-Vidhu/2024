5f9958b354aea8f0f5c34a436da8b6405a1dfb85
SUBIN S <subinvidhu3232@gmail.com>
2024-11-05 08:52:36 +0530

    inpaint

---
+++ DataCamp/Image Processing in Python/3_restoration.py
@@ -0,0 +1,11 @@
+# Restoration
+# Import the module from restoration
+from skimage.restoration import inpaint
+
+# Show the defective image
+show_image(defect_image, 'Image to restore')
+
+# Apply the restoration function to the image using the mask(assuming the mask is already created)
+restored_image = inpaint.inpaint_biharmonic(defect_image, mask, multichannel=True)
+show_image(restored_image)
+
