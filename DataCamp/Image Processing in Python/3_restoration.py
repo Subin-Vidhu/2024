d9a4db655dd804951b0721703a8858acc3ecff8a
SUBIN S <subinvidhu3232@gmail.com>
2024-11-05 09:32:36 +0530

    counting

--- DataCamp/Image Processing in Python/3_restoration.py
+++ DataCamp/Image Processing in Python/3_restoration.py
@@ -79,3 +79,18 @@ contours = measure.find_contours(binary, 0.8)
 
 # Show the image
 show_image_contour(image_dice, contours)
+
+# Create list with the shape of each contour
+shape_contours = [cnt.shape[0] for cnt in contours]
+
+# Set 50 as the maximum size of the dots shape
+max_dots_shape = 50
+
+# Count dots in contours excluding bigger than dots size
+dots_contours = [cnt for cnt in contours if np.shape(cnt)[0] < max_dots_shape]
+
+# Shows all contours found 
+show_image_contour(binary, contours)
+
+# Print the dice's number
+print("Dice's dots number: {}. ".format(len(dots_contours)))
