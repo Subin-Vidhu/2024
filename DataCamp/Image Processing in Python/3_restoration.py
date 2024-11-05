fbc05ffc68dfb655fd254aec0e9562c98de4e81e
SUBIN S <subinvidhu3232@gmail.com>
2024-11-05 08:57:21 +0530

    Removing logos

--- DataCamp/Image Processing in Python/3_restoration.py
+++ DataCamp/Image Processing in Python/3_restoration.py
@@ -9,3 +9,16 @@ show_image(defect_image, 'Image to restore')
 restored_image = inpaint.inpaint_biharmonic(defect_image, mask, multichannel=True)
 show_image(restored_image)
 
+# Removing image
+# Initialize the mask
+mask = np.zeros(image_with_logo.shape[:-1])
+
+# Set the pixels where the logo is to 1
+mask[210:290, 360:425] = 1
+
+# Apply inpainting to remove the logo
+image_logo_removed = inpaint.inpaint_biharmonic(image_with_logo,mask,multichannel=True)
+
+# Show the original and logo removed images
+show_image(image_with_logo, 'Image with logo')
+show_image(image_logo_removed, 'Image with logo removed')
