85fb97f93eb7383da6b2a424174febe14f535a20
SUBIN S <subinvidhu3232@gmail.com>
2024-11-05 11:13:08 +0530

    Perspective

--- DataCamp/Image Processing in Python/4_edges.py
+++ DataCamp/Image Processing in Python/4_edges.py
@@ -20,3 +20,20 @@ edges_2_2 = canny(grapefruit, sigma=2.2)
 # Show resulting images
 show_image(edges_1_8, "Sigma of 1.8")
 show_image(edges_2_2, "Sigma of 2.2")
+
+# Perspective
+# Import the corner detector related functions and module
+from skimage.feature import corner_harris, corner_peaks
+
+# Convert image from RGB-3 to grayscale
+building_image_gray = color.rgb2gray(building_image)
+
+# Apply the detector  to measure the possible corners
+measure_image = corner_harris(building_image_gray)
+
+# Find the peaks of the corners using the Harris detector
+coords = corner_peaks(measure_image, min_distance=20, threshold_rel=0.02)
+
+# Show original and resulting image with corners detected
+show_image(building_image, "Original")
+show_image_with_corners(building_image, coords)
