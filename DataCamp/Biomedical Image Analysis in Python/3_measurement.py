cfa620d64cc80dfb9729d673cc0ce615aff07ee1
Subin-Vidhu <subinvidhu3232@gmail.com>
2024-11-05 19:45:33 +0530

    Distance

--- DataCamp/Biomedical Image Analysis in Python/3_measurement.py
+++ DataCamp/Biomedical Image Analysis in Python/3_measurement.py
@@ -100,3 +100,18 @@ voxel_size_mm3 = vol.meta['sampling'][0] * vol.meta['sampling'][1] * vol.meta['s
 volume_mm3 = num_voxels * voxel_size_mm3
 
 print(f"The volume of the left ventricle is {volume_mm3} mm^3")
+
+
+# Distance
+# Calculate left ventricle distances
+lv = np.where(labels == 1, 1, 0)
+dists = ndi.distance_transform_edt(lv, sampling=vol.meta['sampling'])
+
+# Report on distances
+print('Max distance (mm):', ndi.maximum(dists))
+print('Max location:', ndi.maximum_position(dists))
+
+# Plot overlay of distances
+overlay = np.where(dists[5] > 0, dists[5], np.nan) 
+plt.imshow(overlay, cmap='hot')
+format_and_render_plot()
