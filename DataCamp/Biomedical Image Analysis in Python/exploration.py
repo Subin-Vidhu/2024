4250c678886cd8a3daee6cd682266e35c3db6b4a
SUBIN S <subinvidhu3232@gmail.com>
2024-11-05 14:32:35 +0530

    im.meta.keys()

--- DataCamp/Biomedical Image Analysis in Python/exploration.py
+++ DataCamp/Biomedical Image Analysis in Python/exploration.py
@@ -8,3 +8,11 @@ im = imageio.imread("chest-220.dcm")
 # Print image attributes
 print('Image type:', type(im))
 print('Shape of image array:', im.shape)
+
+#2
+# Import ImageIO
+import imageio
+im = imageio.imread('chest-220.dcm')
+
+# Print the available metadata fields
+print(im.meta.keys())
