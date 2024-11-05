1b7511238a38ff4130983368e5e8d152179dc0b3
SUBIN S <subinvidhu3232@gmail.com>
2024-11-05 14:39:37 +0530

    plot using matplotlib

--- DataCamp/Biomedical Image Analysis in Python/exploration.py
+++ DataCamp/Biomedical Image Analysis in Python/exploration.py
@@ -16,3 +16,16 @@ im = imageio.imread('chest-220.dcm')
 
 # Print the available metadata fields
 print(im.meta.keys())
+
+# Print the image type
+print('Image type:', type(im))
+
+#3
+# Plot the image
+plt.imshow(im, cmap='gray')
+plt.axis('off')
+plt.show()
+
+#4
+# Plot the image with vmin, vmax and cmap - 'gray', here vmin is 40 and vmax is 80, vmin and vmax set the limits of the color scale, meaning that all pixels with values lower than vmin will be black and all pixels with values higher than vmax will be white.
+plt.imshow(im, cmap='gray', vmin=40, vmax=80)
