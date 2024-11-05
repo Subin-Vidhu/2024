6865d7fb4fd28bba5758d664a05b7420ef9515b9
SUBIN S <subinvidhu3232@gmail.com>
2024-11-05 17:06:31 +0530

    Filter and Detect Edges

--- DataCamp/Biomedical Image Analysis in Python/2_mask_and_filters.py
+++ DataCamp/Biomedical Image Analysis in Python/2_mask_and_filters.py
@@ -82,3 +82,31 @@ fig, axes = plt.subplots(1,2)
 axes[0].imshow(im, cmap='gray')
 axes[1].imshow(im_filt, cmap='gray')
 format_and_render_plot()
+
+# Filter functions
+# Smoothing
+# Smooth "im" with Gaussian filters
+# Smooth "im" with Gaussian filters
+im_s1 = ndi.gaussian_filter(im, sigma=1)
+im_s3 = ndi.gaussian_filter(im, sigma=3)
+
+# Draw bone masks of each image
+fig, axes = plt.subplots(1,3)
+axes[0].imshow(im >= 145)
+axes[1].imshow(im_s1>= 145)
+axes[2].imshow(im_s3>= 145)
+format_and_render_plot()
+
+# Detect Edges
+# Set weights to detect vertical edges
+weights = [[+1, 0, -1],
+           [+1, 0, -1],
+           [+1, 0, -1]]
+
+# Convolve "im" with filter weights
+edges = ndi.convolve(im, weights)
+
+# Draw the image in color
+plt.imshow(edges, cmap="seismic", vmin=-150, vmax=150)
+plt.colorbar()
+format_and_render_plot()
