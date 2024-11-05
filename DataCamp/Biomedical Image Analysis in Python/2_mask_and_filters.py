e566a9d9e3651b04cf40044a3a4b26e465bf2a10
SUBIN S <subinvidhu3232@gmail.com>
2024-11-05 16:12:16 +0530

    Creating Masks

--- DataCamp/Biomedical Image Analysis in Python/2_mask_and_filters.py
+++ DataCamp/Biomedical Image Analysis in Python/2_mask_and_filters.py
@@ -26,3 +26,14 @@ fig, axes = plt.subplots(2, 1, sharex=True)
 axes[0].plot(hist, label='Histogram')
 axes[1].plot(cdf, label='CDF')
 format_and_render_plot()
+
+# Creating Masks
+# Create skin and bone masks
+mask_bone = im >= 145
+mask_skin = (im >= 45) & (im < 145)
+
+# Plot the skin (0) and bone (1) masks
+fig, axes = plt.subplots(1,2)
+axes[0].imshow(mask_skin, cmap='gray')
+axes[1].imshow(mask_bone, cmap='gray')
+format_and_render_plot()
