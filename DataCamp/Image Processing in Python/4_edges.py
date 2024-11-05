6a5617b9654375cf95d149024b0ec3d1d075c659
SUBIN S <subinvidhu3232@gmail.com>
2024-11-05 11:07:42 +0530

    Canny edge detector

---
+++ DataCamp/Image Processing in Python/4_edges.py
@@ -0,0 +1,22 @@
+# Canny edge detector
+# Import the canny edge detector 
+from skimage.feature import canny
+
+# Convert image to grayscale
+grapefruit = color.rgb2gray(grapefruit)
+
+# Apply canny edge detector
+canny_edges = canny(grapefruit)
+
+# Show resulting image
+show_image(canny_edges, "Edges with Canny")
+
+# Apply canny edge detector with a sigma of 1.8
+edges_1_8 = canny(grapefruit, sigma=1.8)
+
+# Apply canny edge detector with a sigma of 2.2
+edges_2_2 = canny(grapefruit, sigma=2.2)
+
+# Show resulting images
+show_image(edges_1_8, "Sigma of 1.8")
+show_image(edges_2_2, "Sigma of 2.2")
