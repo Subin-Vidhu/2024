8fa00b302729d831cf6751ed52d698f009284724
SUBIN S <subinvidhu3232@gmail.com>
2024-11-05 11:33:08 +0530

    Privacy Protection

--- DataCamp/Image Processing in Python/4_edges.py
+++ DataCamp/Image Processing in Python/4_edges.py
@@ -99,3 +99,20 @@ detected = detector.detect_multi_scale(img=segmented_image,
 
 # Show the detected faces
 show_detected_face(segmented_image, detected)
+
+# Privacy Protection
+# Detect the faces
+detected = detector.detect_multi_scale(img=group_image, 
+                                       scale_factor=1.2, step_ratio=1, 
+                                       min_size=(10,10), max_size=(100, 100))
+# For each detected face
+for d in detected:  
+    # Obtain the face rectangle from detected coordinates
+    face = getFaceRectangle(d)
+    
+    # Apply gaussian filter to extracted face
+    blurred_face = gaussian(face, multichannel=True, sigma = 8)
+    
+    # Merge this blurry face to our final image and show it
+    resulting_image = mergeBlurryFace(group_image, blurred_face) 
+show_image(resulting_image, "Blurred faces")
