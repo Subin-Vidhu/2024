f6c8ebb7ce52c026ef86995e1a5aae6389337d20
SUBIN S <subinvidhu3232@gmail.com>
2024-11-08 14:56:02 +0530

    Create 4_Fine-tuning keras models.py

---
+++ DataCamp/Introduction to Deep Learning in Python/4_Fine-tuning keras models.py
@@ -0,0 +1,191 @@
+'''
+Changing optimization parameters
+It's time to get your hands dirty with optimization. You'll now try optimizing a model at a very low learning rate, a very high learning rate, and a "just right" learning rate. You'll want to look at the results after running this exercise, remembering that a low value for the loss function is good.
+
+For these exercises, we've pre-loaded the predictors and target values from your previous classification models (predicting who would survive on the Titanic). You'll want the optimization to start from scratch every time you change the learning rate, to give a fair comparison of how each learning rate did in your results. So we have created a function get_new_model() that creates an unoptimized model to optimize.
+'''
+
+# Import the SGD optimizer
+from tensorflow.keras.optimizers import SGD
+
+# Create list of learning rates: lr_to_test
+lr_to_test = [.000001, 0.01, 1]
+
+# Loop over learning rates
+for lr in lr_to_test:
+    print('\n\nTesting model with learning rate: %f\n'%lr )
+    
+    # Build new model to test, unaffected by previous models
+    model = get_new_model()
+    
+    # Create SGD optimizer with specified learning rate: my_optimizer
+    my_optimizer = SGD(lr=lr)
+    
+    # Compile the model
+    model.compile(optimizer=my_optimizer, loss='categorical_crossentropy')
+    
+    # Fit the model
+    model.fit(predictors, target)
+
+'''
+Evaluating model accuracy on validation dataset
+Now it's your turn to monitor model accuracy with a validation data set. A model definition has been provided as model. Your job is to add the code to compile it and then fit it. You'll check the validation score in each epoch.
+'''
+
+# Save the number of columns in predictors: n_cols
+n_cols = predictors.shape[1]
+input_shape = (n_cols,)
+
+# Specify the model
+model = Sequential()
+model.add(Dense(100, activation='relu', input_shape = input_shape))
+model.add(Dense(100, activation='relu'))
+model.add(Dense(2, activation='softmax'))
+
+# Compile the model
+model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+
+# Fit the model
+hist = model.fit(predictors, target, validation_split=0.3)
+
+'''
+Early stopping: Optimizing the optimization
+Now that you know how to monitor your model performance throughout optimization, you can use early stopping to stop optimization when it isn't helping any more. Since the optimization stops automatically when it isn't helping, you can also set a high value for epochs in your call to .fit(), as Dan showed in the video.
+
+The model you'll optimize has been specified as model. As before, the data is pre-loaded as predictors and target.
+'''
+
+# Import EarlyStopping
+from tensorflow.keras.callbacks import EarlyStopping
+
+# Save the number of columns in predictors: n_cols
+n_cols = predictors.shape[1]
+input_shape = (n_cols,)
+
+# Specify the model
+model = Sequential()
+model.add(Dense(100, activation='relu', input_shape = input_shape))
+model.add(Dense(100, activation='relu'))
+model.add(Dense(2, activation='softmax'))
+
+# Compile the model
+model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+
+# Define early_stopping_monitor
+early_stopping_monitor = EarlyStopping(patience=2)
+
+# Fit the model
+model.fit(predictors, target, epochs=30, validation_split=0.3, callbacks=[early_stopping_monitor])
+
+'''
+Experimenting with wider networks
+Now you know everything you need to begin experimenting with different models!
+
+A model called model_1 has been pre-loaded. You can see a summary of this model printed in the IPython Shell. This is a relatively small network, with only 10 units in each hidden layer.
+
+In this exercise you'll create a new model called model_2 which is similar to model_1, except it has 100 units in each hidden layer.
+
+After you create model_2, both models will be fitted, and a graph showing both models loss score at each epoch will be shown. We added the argument verbose=False in the fitting commands to print out fewer updates, since you will look at these graphically instead of as text.
+
+Because you are fitting two models, it will take a moment to see the outputs after you hit run, so be patient.
+'''
+
+# Define early_stopping_monitor
+early_stopping_monitor = EarlyStopping(patience=2)
+
+# Create the new model: model_2
+model_2 = Sequential()
+
+# Add the first and second layers
+model_2.add(Dense(100, activation='relu', input_shape=input_shape))
+model_2.add(Dense(100, activation='relu'))
+
+# Add the output layer
+model_2.add(Dense(2, activation='softmax'))
+
+# Compile model_2
+model_2.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+
+# Fit model_1
+model_1_training = model_1.fit(predictors, target, epochs=15, validation_split=0.2, callbacks=[early_stopping_monitor], verbose=False)
+
+# Fit model_2
+model_2_training = model_2.fit(predictors, target, epochs=15, validation_split=0.2, callbacks=[early_stopping_monitor], verbose=False)
+
+# Create the plot
+plt.plot(model_1_training.history['val_loss'], 'r', model_2_training.history['val_loss'], 'b')
+plt.xlabel('Epochs')
+plt.ylabel('Validation score')
+plt.show()
+
+'''
+Adding layers to a network
+You've seen how to experiment with wider networks. In this exercise, you'll try a deeper network (more hidden layers).
+
+Once again, you have a baseline model called model_1 as a starting point. It has 1 hidden layer, with 10 units. You can see a summary of that model's structure printed out. You will create a similar network with 3 hidden layers (still keeping 10 units in each layer).
+
+This will again take a moment to fit both models, so you'll need to wait a few seconds to see the results after you run your code.
+'''
+
+# The input shape to use in the first hidden layer
+input_shape = (n_cols,)
+
+# Create the new model: model_2
+model_2 = Sequential()
+
+# Add the first, second, and third hidden layers
+model_2.add(Dense(10, activation='relu', input_shape=input_shape))
+model_2.add(Dense(10, activation='relu'))
+model_2.add(Dense(10, activation='relu'))
+
+# Add the output layer
+model_2.add(Dense(2, activation='softmax'))
+
+# Compile model_2
+model_2.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+
+# Fit model 1
+model_1_training = model_1.fit(predictors, target, epochs=15, validation_split=0.2, verbose=False)
+
+# Fit model 2
+model_2_training = model_2.fit(predictors, target, epochs=15, validation_split=0.2, verbose=False)
+
+# Create the plot
+plt.plot(model_1_training.history['val_loss'], 'r', model_2_training.history['val_loss'], 'b')
+plt.xlabel('Epochs')
+plt.ylabel('Validation score')
+plt.show()
+
+'''
+Building your own digit recognition model
+You've reached the final exercise of the course - you now know everything you need to build an accurate model to recognize handwritten digits!
+
+We've already done the basic manipulation of the MNIST dataset shown in the video, so you have X and y loaded and ready to model with. Sequential and Dense from tensorflow.keras are also pre-imported.
+
+To add an extra challenge, we've loaded only 2500 images, rather than 60000 which you will see in some published results. Deep learning models perform better with more data, however, they also take longer to train, especially when they start becoming more complex.
+
+If you have a computer with a CUDA compatible GPU, you can take advantage of it to improve computation time. If you don't have a GPU, no problem! You can set up a deep learning environment in the cloud that can run your models on a GPU. Here is a blog post by Dan that explains how to do this - check it out after completing this exercise! It is a great next step as you continue your deep learning journey.
+
+Ready to take your deep learning to the next level? Check out Advanced Deep Learning with Keras to see how the Keras functional API lets you build domain knowledge to solve new types of problems. Once you know how to use the functional API, take a look at Image Processing with Keras in Python to learn image-specific applications of Keras.
+'''
+
+# Create the model: model
+model = Sequential()
+
+# Add the first hidden layer
+model.add(Dense(50, activation='relu', input_shape=(784,)))
+
+# Add the second hidden layer
+model.add(Dense(50, activation='relu'))
+
+# Add the output layer
+model.add(Dense(10, activation='softmax'))
+
+# Compile the model
+model.compile(optimizer='adam',
+              loss='categorical_crossentropy',
+              metrics=['accuracy'])
+
+# Fit the model
+model.fit(X, y, validation_split=0.3, epochs=10)
+
