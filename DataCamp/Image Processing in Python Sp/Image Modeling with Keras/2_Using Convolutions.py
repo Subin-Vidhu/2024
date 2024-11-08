17efb252fc5f5534f4c59c60db7605223a91fdab
SUBIN S <subinvidhu3232@gmail.com>
2024-11-08 12:05:22 +0530

    Create 2_Using Convolutions.py

---
+++ DataCamp/Image Processing in Python Sp/Image Modeling with Keras/2_Using Convolutions.py
@@ -0,0 +1,102 @@
+array = np.array([1, 0, 1, 0, 1, 0, 1, 0, 1, 0])
+kernel = np.array([1, -1, 0])
+conv = np.array([0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+
+# Output array
+for ii in range(8):
+    conv[ii] = (kernel * array[ii:ii+3]).sum()
+
+# Print conv
+print(conv)
+
+
+kernel = np.array([[0, 1, 0], [1, 1, 1], [0, 1, 0]])
+result = np.zeros(im.shape)
+
+# Output array
+for ii in range(im.shape[0] - 3):
+    for jj in range(im.shape[1] - 3):
+        result[ii, jj] = (im[ii:ii+3, jj:jj+3] * kernel).sum()
+
+# Print result
+print(result)
+
+'''
+Convolutional network for image classification
+Convolutional networks for classification are constructed from a sequence of convolutional layers (for image processing) and fully connected (Dense) layers (for readout). In this exercise, you will construct a small convolutional network for classification of the data from the fashion dataset.
+'''
+# Import the necessary components from Keras
+from keras.models import Sequential
+from keras.layers import Dense, Conv2D, Flatten
+
+# Initialize the model object
+model = Sequential()
+
+# Add a convolutional layer
+model.add(Conv2D(10, kernel_size=3, activation='relu', 
+                 input_shape=(img_rows, img_cols, 1)))
+
+# Flatten the output of the convolutional layer
+model.add(Flatten())
+# Add an output layer for the 3 categories
+model.add(Dense(3, activation='softmax'))
+
+'''
+Training a CNN to classify clothing types
+Before training a neural network it needs to be compiled with the right cost function, using the right optimizer. During compilation, you can also define metrics that the network calculates and reports in every epoch. Model fitting requires a training data set, together with the training labels to the network.
+
+The Conv2D model you built in the previous exercise is available in your workspace.
+'''
+
+# Compile the model 
+model.compile(optimizer='adam', 
+              loss='categorical_crossentropy', 
+              metrics=['accuracy'])
+
+# Fit the model on a training set
+model.fit(train_data, train_labels, 
+          validation_split=0.2, 
+          epochs=3, batch_size=10)
+
+'''
+Evaluating a CNN with test data
+To evaluate a trained neural network, you should provide a separate testing data set of labeled images. The model you fit in the previous exercise is available in your workspace.
+'''
+
+# Evaluate the model on separate test data
+model.evaluate(test_data, test_labels, batch_size=10)
+
+'''
+Add padding to a CNN
+Padding allows a convolutional layer to retain the resolution of the input into this layer. This is done by adding zeros around the edges of the input image, so that the convolution kernel can overlap with the pixels on the edge of the image.
+'''
+
+# Initialize the model
+model = Sequential()
+
+# Add the convolutional layer
+model.add(Conv2D(10, kernel_size=3, activation='relu', 
+                 input_shape=(img_rows, img_cols, 1), 
+                 padding='same'))
+
+# Feed into output layer
+model.add(Flatten())
+model.add(Dense(3, activation='softmax'))
+
+'''
+Add strides to a convolutional network
+The size of the strides of the convolution kernel determines whether the kernel will skip over some of the pixels as it slides along the image. This affects the size of the output because when strides are larger than one, the kernel will be centered on only some of the pixels.
+'''
+
+# Initialize the model
+model = Sequential()
+
+# Add a convolutional layer
+model.add(Conv2D(10, kernel_size=3, activation='relu', 
+                 input_shape=(img_rows, img_cols, 1), 
+                 strides=2))
+
+# Feed into output layer
+model.add(Flatten())
+model.add(Dense(3, activation='softmax'))
+
