5c0da3907715ae4d00fe5e646559a500d2ecd630
Subin-Vidhu <subinvidhu3232@gmail.com>
2024-11-09 19:48:34 +0530

    Create 3_Operational Phase.py

---
+++ DataCamp/Associate AI Engineer for Developers/LLMOps Concepts/3_Operational Phase.py
@@ -0,0 +1,6 @@
+"""Prompt compression
+Prompt compression is a strategy falling under the broader category of optimizing prompts. Your goal is to reduce costs while keeping the response as good as possible. The LLM that you are using charges based on token count."""
+
+"""Mitigation strategies
+There are different ways to mitigate risks depending on the threats your LLM application might face. Thus, it's important to know which strategy works best for which threat. For each mitigation strategy, figure out which threat it aims to tackle: prompt injection, data poisoning, or denial-of-service."""
+
